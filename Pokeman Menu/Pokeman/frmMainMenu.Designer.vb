4585cb148129d886c0210ecabf3557611c6cc41c
Awedtan <31903483+Awedtan@users.noreply.github.com>
2018-12-09 10:01:39 -0500

    Add files via upload

---
+++ Pokeman Menu/Pokeman/frmMainMenu.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.btnBattle = New System.Windows.Forms.Button()
+		Me.btnStore = New System.Windows.Forms.Button()
+		Me.btnBox = New System.Windows.Forms.Button()
+		Me.lblPokeman1 = New System.Windows.Forms.Label()
+		Me.lblPokeman2 = New System.Windows.Forms.Label()
+		Me.lblPokeman3 = New System.Windows.Forms.Label()
+		Me.lblPokeman4 = New System.Windows.Forms.Label()
+		Me.lblPokeman5 = New System.Windows.Forms.Label()
+		Me.lblPokeman6 = New System.Windows.Forms.Label()
+		Me.lblBall = New System.Windows.Forms.Label()
+		Me.lblCurrency = New System.Windows.Forms.Label()
+		Me.lblLotion = New System.Windows.Forms.Label()
+		Me.lblMedpack = New System.Windows.Forms.Label()
+		Me.lblFeast = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'btnBattle
+		'
+		Me.btnBattle.Location = New System.Drawing.Point(339, 223)
+		Me.btnBattle.Name = "btnBattle"
+		Me.btnBattle.Size = New System.Drawing.Size(110, 56)
+		Me.btnBattle.TabIndex = 0
+		Me.btnBattle.Text = "Battle!"
+		Me.btnBattle.UseVisualStyleBackColor = True
+		'
+		'btnStore
+		'
+		Me.btnStore.Location = New System.Drawing.Point(614, 177)
+		Me.btnStore.Name = "btnStore"
+		Me.btnStore.Size = New System.Drawing.Size(110, 36)
+		Me.btnStore.TabIndex = 1
+		Me.btnStore.Text = "Store"
+		Me.btnStore.UseVisualStyleBackColor = True
+		'
+		'btnBox
+		'
+		Me.btnBox.Location = New System.Drawing.Point(614, 286)
+		Me.btnBox.Name = "btnBox"
+		Me.btnBox.Size = New System.Drawing.Size(110, 36)
+		Me.btnBox.TabIndex = 2
+		Me.btnBox.Text = "Pokemon Box"
+		Me.btnBox.UseVisualStyleBackColor = True
+		'
+		'lblPokeman1
+		'
+		Me.lblPokeman1.AutoSize = True
+		Me.lblPokeman1.Location = New System.Drawing.Point(74, 261)
+		Me.lblPokeman1.Name = "lblPokeman1"
+		Me.lblPokeman1.Size = New System.Drawing.Size(61, 13)
+		Me.lblPokeman1.TabIndex = 3
+		Me.lblPokeman1.Text = "Pokeman 1"
+		'
+		'lblPokeman2
+		'
+		Me.lblPokeman2.AutoSize = True
+		Me.lblPokeman2.Location = New System.Drawing.Point(74, 286)
+		Me.lblPokeman2.Name = "lblPokeman2"
+		Me.lblPokeman2.Size = New System.Drawing.Size(61, 13)
+		Me.lblPokeman2.TabIndex = 4
+		Me.lblPokeman2.Text = "Pokeman 2"
+		'
+		'lblPokeman3
+		'
+		Me.lblPokeman3.AutoSize = True
+		Me.lblPokeman3.Location = New System.Drawing.Point(74, 312)
+		Me.lblPokeman3.Name = "lblPokeman3"
+		Me.lblPokeman3.Size = New System.Drawing.Size(61, 13)
+		Me.lblPokeman3.TabIndex = 5
+		Me.lblPokeman3.Text = "Pokeman 3"
+		'
+		'lblPokeman4
+		'
+		Me.lblPokeman4.AutoSize = True
+		Me.lblPokeman4.Location = New System.Drawing.Point(74, 339)
+		Me.lblPokeman4.Name = "lblPokeman4"
+		Me.lblPokeman4.Size = New System.Drawing.Size(61, 13)
+		Me.lblPokeman4.TabIndex = 6
+		Me.lblPokeman4.Text = "Pokeman 4"
+		'
+		'lblPokeman5
+		'
+		Me.lblPokeman5.AutoSize = True
+		Me.lblPokeman5.Location = New System.Drawing.Point(74, 365)
+		Me.lblPokeman5.Name = "lblPokeman5"
+		Me.lblPokeman5.Size = New System.Drawing.Size(61, 13)
+		Me.lblPokeman5.TabIndex = 7
+		Me.lblPokeman5.Text = "Pokeman 5"
+		'
+		'lblPokeman6
+		'
+		Me.lblPokeman6.AutoSize = True
+		Me.lblPokeman6.Location = New System.Drawing.Point(74, 391)
+		Me.lblPokeman6.Name = "lblPokeman6"
+		Me.lblPokeman6.Size = New System.Drawing.Size(61, 13)
+		Me.lblPokeman6.TabIndex = 8
+		Me.lblPokeman6.Text = "Pokeman 6"
+		'
+		'lblBall
+		'
+		Me.lblBall.AutoSize = True
+		Me.lblBall.Location = New System.Drawing.Point(75, 82)
+		Me.lblBall.Name = "lblBall"
+		Me.lblBall.Size = New System.Drawing.Size(75, 13)
+		Me.lblBall.TabIndex = 9
+		Me.lblBall.Text = "# of Pokeballs"
+		'
+		'lblCurrency
+		'
+		Me.lblCurrency.AutoSize = True
+		Me.lblCurrency.Location = New System.Drawing.Point(55, 30)
+		Me.lblCurrency.Name = "lblCurrency"
+		Me.lblCurrency.Size = New System.Drawing.Size(49, 13)
+		Me.lblCurrency.TabIndex = 10
+		Me.lblCurrency.Text = "Currency"
+		'
+		'lblLotion
+		'
+		Me.lblLotion.AutoSize = True
+		Me.lblLotion.Location = New System.Drawing.Point(75, 105)
+		Me.lblLotion.Name = "lblLotion"
+		Me.lblLotion.Size = New System.Drawing.Size(83, 13)
+		Me.lblLotion.TabIndex = 11
+		Me.lblLotion.Text = "# of Little Lotion"
+		'
+		'lblMedpack
+		'
+		Me.lblMedpack.AutoSize = True
+		Me.lblMedpack.Location = New System.Drawing.Point(74, 128)
+		Me.lblMedpack.Name = "lblMedpack"
+		Me.lblMedpack.Size = New System.Drawing.Size(114, 13)
+		Me.lblMedpack.TabIndex = 12
+		Me.lblMedpack.Text = "# of Medium Medpack"
+		'
+		'lblFeast
+		'
+		Me.lblFeast.AutoSize = True
+		Me.lblFeast.Location = New System.Drawing.Point(75, 152)
+		Me.lblFeast.Name = "lblFeast"
+		Me.lblFeast.Size = New System.Drawing.Size(74, 13)
+		Me.lblFeast.TabIndex = 13
+		Me.lblFeast.Text = "# of Full Feast"
+		'
+		'MainMenu
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.Controls.Add(Me.lblFeast)
+		Me.Controls.Add(Me.lblMedpack)
+		Me.Controls.Add(Me.lblLotion)
+		Me.Controls.Add(Me.lblCurrency)
+		Me.Controls.Add(Me.lblBall)
+		Me.Controls.Add(Me.lblPokeman6)
+		Me.Controls.Add(Me.lblPokeman5)
+		Me.Controls.Add(Me.lblPokeman4)
+		Me.Controls.Add(Me.lblPokeman3)
+		Me.Controls.Add(Me.lblPokeman2)
+		Me.Controls.Add(Me.lblPokeman1)
+		Me.Controls.Add(Me.btnBox)
+		Me.Controls.Add(Me.btnStore)
+		Me.Controls.Add(Me.btnBattle)
+		Me.Name = "MainMenu"
+		Me.Text = "Form1"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents btnBattle As Button
+    Friend WithEvents btnStore As Button
+    Friend WithEvents btnBox As Button
+    Friend WithEvents lblPokeman1 As Label
+    Friend WithEvents lblPokeman2 As Label
+    Friend WithEvents lblPokeman3 As Label
+    Friend WithEvents lblPokeman4 As Label
+    Friend WithEvents lblPokeman5 As Label
+    Friend WithEvents lblPokeman6 As Label
+    Friend WithEvents lblBall As Label
+    Friend WithEvents lblCurrency As Label
+    Friend WithEvents lblLotion As Label
+    Friend WithEvents lblMedpack As Label
+    Friend WithEvents lblFeast As Label
+End Class
